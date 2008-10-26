18d136aa169f7dd8e59766adbbb09efbc0df4926
Eloy Duran <eloy.de.enige@gmail.com>
2008-10-26 14:21:53 +0100

    Added NZB ::queue and ::request_file.

--- lib/nzb.rb
+++ lib/nzb.rb
@@ -1,16 +1,34 @@
 require 'nzb/parser'
 
 class NZB
-  attr_reader :path, :files, :queue, :processing
+  class << self
+    def queued
+      @queue ||= []
+    end
+    
+    def queue(path)
+      queued << new(path)
+    end
+    
+    def clear_queue!
+      @queue = []
+    end
+    
+    # This is going to be called by the connection(s).
+    def request_file
+      queued.first.request_file
+    end
+  end
+  
+  attr_reader :path, :files, :queue
   
   def initialize(path)
     @path = path
     @queue = (@files = Parser.new(@path).files).dup
   end
   
-  # This is going to be called by the connection.
-  def request_segment_job
-    @processing ||= @queue.shift
-    @processing.request_job
+  # This is called by NZB.request_file
+  def request_file
+    @queue.shift
   end
 end
--- test/nzb_test.rb
+++ test/nzb_test.rb
@@ -1,7 +1,30 @@
 require File.expand_path('../test_helper', __FILE__)
 require 'nzb'
 
-describe "NZB" do
+describe "NZB class" do
+  before do
+    NZB.queue fixture('ubuntu.nzb')
+  end
+  
+  after do
+    NZB.clear_queue!
+  end
+  
+  it "should have added a new NZB to the queue" do
+    NZB.queued.last.path.should == fixture('ubuntu.nzb')
+  end
+  
+  it "should clear the queue" do
+    NZB.clear_queue!
+    NZB.queued.should.be.empty
+  end
+  
+  it "should return a file" do
+    NZB.request_file.should == NZB.queued.last.files.first
+  end
+end
+
+describe "NZB instance" do
   before do
     @nzb = NZB.new(fixture('ubuntu.nzb'))
   end
@@ -15,10 +38,9 @@ describe "NZB" do
     (@nzb.files.first.segments.length + @nzb.files.last.segments.length).should == 202
   end
   
-  it "should return a segment to be downloaded mark the file as “processing”" do
-    @nzb.request_segment_job.should == @nzb.files.first.segments.first
+  it "should return a file to be downloaded from the queue" do
+    @nzb.request_file.should == @nzb.files.first
     @nzb.queue.should == [@nzb.files.last]
-    @nzb.processing.should == @nzb.files.first
   end
   
   xit "should advance to the next file if the current one is done" do
